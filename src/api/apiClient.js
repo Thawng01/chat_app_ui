c10aa6d71405a1ef07d26fc9fef57ad38ec5c0b5
thechin press <thechinpress@gmail.com>
2022-04-12 14:51:44 +0630

    Deploy to netlify

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -5,7 +5,7 @@ export const source = axios.CancelToken.source();
 const token = localStorage.getItem("token");
 
 const apiClient = axios.create({
-    baseURL: "http://192.168.43.240:9000/api",
+    baseURL: "https://node-chat-app-api.herokuapp.com/api",
     headers: { "x-auth-token": token },
 });
 
